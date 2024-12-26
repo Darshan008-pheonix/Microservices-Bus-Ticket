0f4393bb7afb02af733a692d0fba97415fd6d5f3
darshan-dn <darshan.dn.keytourist@gmail.com>
2024-12-26 13:36:46 +0530

    service outline

---
+++ Booking-Service/src/test/java/com/findroute/book/BookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findroute.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Tickets-Inventory-Service/src/main/java/com/findroute/inventory/TicketsInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.findroute.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsInventoryServiceApplication.class, args);
+	}
+
+}
